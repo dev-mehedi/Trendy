949b46fca936bfc9397118375a6f6ca8bfea72ac
Md Mahmudur Rahman <dev-mehedi@users.noreply.github.com>
2024-05-28 09:59:52 +0300

    Trendy

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+// owl-carousel
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop: true,
+  	autoplay: 1000,
+  	nav: true,
+  	navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+  });
+});
